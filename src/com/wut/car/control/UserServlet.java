b5c797bae6f316c8d7862ff84643289dc7843b87
Administrator <Administrator@PC-16>
2019-12-30 17:21:50 +0800

    查37:58

--- src/com/wut/car/control/UserServlet.java
+++ src/com/wut/car/control/UserServlet.java
@@ -1,18 +1,22 @@
 package com.wut.car.control;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+import com.wut.car.model.UserDAO;
+
 /**
  * Servlet implementation class UserServlet
  */
 @WebServlet("/UserServlet")
 public class UserServlet extends HttpServlet {
-	
+	private UserDAO dao= new UserDAO();
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request,response);
 		
@@ -25,6 +29,8 @@ public class UserServlet extends HttpServlet {
 			case"listALL":
 			{
 				System.out.println("查询显示有的后台记录");
+				List<User> users=dao.listAll();
+				System.out.println(users.size());
 				break;
 			}
 			case"add":
--- src/com/wut/car/model/UserDAO.java
+++ src/com/wut/car/model/UserDAO.java
@@ -1,23 +1,43 @@
 package com.wut.car.model;
 
+import java.sql.DriverManager;
 import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+
+
+import com.mysql.jdbc.Connection;
 
 public class UserDAO {
 
 	public class CarDAO{
-		public ArrayList<User> listALL(){
-			ArrayList<User> user=new ArrayList<>();
+		
+		
+		public List<User> listALL(){
+			List<User> users=null;
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
-				Connection
-			} catch (Exception e) {
+				Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/user","root","root");
 				
+				QueryRunner run = new QueryRunner();
+
+				ResultSetHandler<List<User>> h = new BeanListHandler<User>(User.class);
+
+				List<User> persons = run.query("SELECT * FROM User", h);
+
+				
+				
+			} catch (Exception e) {
+				users=new ArrayList<>();
 				e.printStackTrace();
 			}
 			
 			
 			
-			return user;
+			return users;
 		}
 	}
 }
